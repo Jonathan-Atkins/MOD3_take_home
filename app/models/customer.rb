9d071e3f4be1f51ef3fc69f352f07922c5af4ea5
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-03-10 10:26:56 -0700

    add: customer model testing. validating email

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,6 @@
 class Customer < ApplicationRecord
+  has_many :subscriptions
+
+  validates :first_name, :last_name, :email, :address, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'is invalid' }
 end
