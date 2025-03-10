ec66144355b534f7cb50ca045aac909cec774b44
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-03-10 12:12:18 -0700

    add: initial model testing and validating complete

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,3 +1,8 @@
 class Subscription < ApplicationRecord
   belongs_to :customer
+  has_many :tea_subscriptions
+  has_many :teas, through: :tea_subscriptions
+
+  validates :title, :price, :cancelled, :frequency, presence: true
+  validates :price, numericality: { greater_than: 0 }
 end
--- spec/models/tea_subscription_spec.rb
+++ spec/models/tea_subscription_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe TeaSubscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:subscription) }
+    it { should belong_to(:tea) }
+  end
+  describe 'validations' do
+    it { should validate_presence_of(:subscription) }
+    it { should validate_presence_of(:tea) }
+  end
 end
